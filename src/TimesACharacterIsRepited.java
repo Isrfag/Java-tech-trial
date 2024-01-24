d7e7935caa334a11bdf692e0fb394d4366a9d7e7
Isrfag <isrfag@gmail.com>
2024-01-24 18:31:23 +0100

    Añadiendo mas pruebas

--- src/TimesACharacterIsRepited.java
+++ src/TimesACharacterIsRepited.java
@@ -1,20 +1,19 @@
 public class TimesACharacterIsRepited {
 
     public static void main (String [] args) {
-        new TimesACharacterIsRepited();
+        new TimesACharacterIsRepited('s');
     }
 
-    public TimesACharacterIsRepited () {
+    public TimesACharacterIsRepited (char character ) {
 
         //2. Dada una cadena de texto cuenta cuantas veces se repite un caracter
 
         String text = "iausioAAduhsdjqaaaddssdssSjjSA889092aa";
         String newText = "";
-        char character = 'a';
         int counter = 0;
         for (int i = 0; i <= text.length() -1 ; i++) {
 
-            if (text.charAt(i) == 'a') {
+            if (text.charAt(i) == character) {
                 counter++;
             }
         }
